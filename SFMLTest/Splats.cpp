ddada8aa59fa33ea7a9eef02b66b903f06067f61
abeacon <alejandro_beacon@hotmail.com>
2024-11-09 14:49:22 -0300

    Removed some commentaries

--- SFMLTest/Splats.cpp
+++ SFMLTest/Splats.cpp
@@ -24,18 +24,8 @@ Splats::~Splats()
 	delete texture;
 }
 
-
-//Por favor, revisar el commit "Splat odd behaviour" - 86d72798729553f99d0fe466349692cd9d64cc71 
-//No entiendo por que tiene ese comportamiento tan extraño, parece como si guardara dos buffer independientes,
-//Me pareció bastante curioso y me gustaría saber el porqué
 void Splats::Update(RenderWindow* app)
 {
-	// Clear the whole texture with red color
-	// Draw stuff to the texture
-
-	// We're done drawing to the texture
-	//texture.display();
-
 	if (Mouse::isButtonPressed(Mouse::Button::Left))
 	{
 		DrawSprite(app, redCircle);
@@ -53,7 +43,6 @@ void Splats::DrawSprite(RenderWindow* app, Texture* tex)
 	sprite->setTexture(*tex);
 	sprite->setOrigin(tex->getSize().x / 2, tex->getSize().y / 2);
 	sprite->setPosition(localPosition.x, localPosition.y);
-	//app->draw(*sprite);
 	texture->draw(*sprite);
 	texture->display();
 }
